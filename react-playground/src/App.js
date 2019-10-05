7d9806fe501b34f2647df8b0775a5df41cec9eb3
Alexander Hoinville <ahoin001@gmail.com>
2019-10-05 14:59:44 -0400

    Turned class component into function component

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,75 +1,125 @@
-import React from 'react';
+// Import useState sow e could use hooks
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends React.Component {
+const App = (props) => {
 
-  // state can  be me manipulated to re render components with new information
-    state = {
-      
-      persons: [
+  // Use state returns an array of two elements and always two elements
+  //1st Element - Current State (Can be updated)
+  //2nd Element - Function to update state and re-render component
 
-        { name: 'Alex', age: 23 },
-        { name: 'Fu', age: 33 },
-        { name: 'Shirlon', age: 19 }
+  // Use destructure to get the elements
+  const [personState, setPersonState] = useState({
 
-      ]
-    
-  }
+    persons: [
 
-  // Method of Class Component
-  switchNameHandler = () => {
+      { name: 'Alex', age: 23 },
+      { name: 'Fu', age: 33 },
+      { name: 'Shirlon', age: 19 }
 
-    console.log('Console log was clicked', console.log("- =- =- =- ", this.state));
+    ],
 
-    // setState Merges a new state object to old state object,
-    // only changing things we are manipulating
-    this.setState({ 
+    otherStateInfo: ""
+
+  })
+
+  const switchNameHandler = () => {
+
+    console.log('Console log was clicked');
+
+    // setPersonState is what we called our function in the hook
+    // It will update the state
+    setPersonState({
+
+      // setState merges setState with our state and will only change what we made changes too 
       persons: [
 
         { name: 'A', age: 23 },
         { name: 'F', age: 33 },
         { name: 'S', age: 19 }
 
-      ] 
+      ]
     })
 
-  }
-
-  render() {
-
-    return (
-
-      /* 
-      
-      1. Class is a reserved word in JSX so we must use className
-      2. Best practice to wrap a component to be returned in 1 div
-      3.
-      */
-      <div className="App">
-
-        <h1> React Playground </h1>
-        <p>  We are from the react app!</p>
-
-        {/* Have button call method on click */}
-        <button onClick={this.switchNameHandler} > Switch Name </button>
-
-        <Person theName={this.state.persons[0].name} theAge={this.state.persons[0].age} />
-        <Person theName={this.state.persons[1].name} theAge={this.state.persons[2].age} />
-        <Person theName={this.state.persons[2].name} theAge={this.state.persons[1].age}> Some Extra Info </Person>
-
-      </div>
-
-
-    );
-
-    // The Javascript above is not html it is jsx, compiled by react behind the scenes
-    // The below jsx is translated to html doing the below
-    // return React.createElement('div',null,'h1','I\'m a React App!!!');
 
   }
 
+  return (
+
+    /* 
+    1. We do not need "this" for states because we are not in a class anymore
+    2. Best practice to wrap a component to be returned in 1 div
+    3.
+    */
+    <div className="App">
+
+      <h1> React Playground </h1>
+      <p>  We are from the react app!</p>
+
+      {/* Have button call method on click */}
+      <button onClick={switchNameHandler} > Switch Name </button>
+
+      <Person
+        theName={personState.persons[0].name}
+        theAge={personState.persons[0].age}
+      />
+
+      <Person
+        theName={personState.persons[1].name}
+        theAge={personState.persons[1].age}
+      />
+
+      <Person
+        theName={personState.persons[2].name}
+        theAge={personState.persons[2].age}> Some Extra Info
+      </Person>
+
+    </div>
+
+  );
+
+  // The Javascript above is not html it is jsx, compiled by react behind the scenes
+  // The below jsx is translated to html doing the below
+  // return React.createElement('div',null,'h1','I\'m a React App!!!');
 
 }
 
 export default App;
+
+// state can  be me manipulated to re render components with new information
+// state = {
+
+//   persons: [
+
+//     { name: 'Alex', age: 23 },
+//     { name: 'Fu', age: 33 },
+//     { name: 'Shirlon', age: 19 }
+
+//   ],
+
+//   otherStateInfo: ""
+
+// }
+
+// // Method of Class Component
+// switchNameHandler = () => {
+
+// console.log('Console log was clicked', console.log("- =- =- =- ", this.state));
+
+// // setState Merges a new state object to old state object,
+// // only changing things we are manipulating
+// this.setState({ 
+
+
+//   // setState merges setState with our state and will only change what we made changes too 
+//   persons: [
+
+//     { name: 'A', age: 23 },
+//     { name: 'F', age: 33 },
+//     { name: 'S', age: 19 }
+
+//   ] 
+// })
+
+// }
