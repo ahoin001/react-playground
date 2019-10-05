02d537d21f90c336ec0cc65169618e1e0d9b5cfc
Alexander Hoinville <ahoin001@gmail.com>
2019-10-05 13:15:08 -0400

    Event handler button that changes names in state

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 
 class App extends React.Component {
 
-  // state cna be me manipulated to re render components with new information
-  state = {
-
-    persons: [
-
-      {name: 'Alex', age: 23},
-      {name: 'Fu', age: 33},
-      {name: 'Shirlon', age: 19}
+  // state can  be me manipulated to re render components with new information
+    state = {
       
-    ]
+      persons: [
+
+        { name: 'Alex', age: 23 },
+        { name: 'Fu', age: 33 },
+        { name: 'Shirlon', age: 19 }
+
+      ]
+    
+  }
+
+  // Method of Class Component
+  switchNameHandler = () => {
+
+    console.log('Console log was clicked', console.log("- =- =- =- ", this.state));
+
+    // setState Merges a new state object to old state object,
+    // only changing things we are manipulating
+    this.setState({ 
+      persons: [
+
+        { name: 'A', age: 23 },
+        { name: 'F', age: 33 },
+        { name: 'S', age: 19 }
+
+      ] 
+    })
 
   }
 
@@ -28,14 +47,17 @@ class App extends React.Component {
       3.
       */
       <div className="App">
-      
+
         <h1> React Playground </h1>
         <p>  We are from the react app!</p>
 
+        {/* Have button call method on click */}
+        <button onClick={this.switchNameHandler} > Switch Name </button>
+
         <Person theName={this.state.persons[0].name} theAge={this.state.persons[0].age} />
         <Person theName={this.state.persons[1].name} theAge={this.state.persons[2].age} />
-        <Person theName={this.state.persons[2].name} theAge={this.state.persons[1].age}> Some Extra Info </Person> 
-      
+        <Person theName={this.state.persons[2].name} theAge={this.state.persons[1].age}> Some Extra Info </Person>
+
       </div>
 
 
